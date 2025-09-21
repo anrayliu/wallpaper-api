7747406afc74d88a7af874aa13c973e23560890a
Anray Liu <anrayliu@gmail.com>
2025-09-21 01:15:01 -0400

    refactor: changed mux field to private

--- internal/server.go
+++ internal/server.go
@@ -13,22 +13,22 @@ func renderJSON[T ResponseError | ResponseSuccess](w http.ResponseWriter, respon
 }
 
 type HttpServer struct {
-	Mux *http.ServeMux
+	mux *http.ServeMux
 }
 
 // sets the route handlers
 
 func (server *HttpServer) initHandlers() {
-	server.Mux.HandleFunc("/get/{file}/", func(w http.ResponseWriter, r *http.Request) {
+	server.mux.HandleFunc("/get/{file}/", func(w http.ResponseWriter, r *http.Request) {
 		fileName := r.PathValue("file")
 		renderJSON(w, ResponseSuccess{"hello world: " + fileName})
 	})
 
-	server.Mux.HandleFunc("/get/", func(w http.ResponseWriter, r *http.Request) {
+	server.mux.HandleFunc("/get/", func(w http.ResponseWriter, r *http.Request) {
 		renderJSON(w, NewResponse400())
 	})
 
-	server.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	server.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		renderJSON(w, NewResponse404())
 	})
 }
@@ -36,10 +36,12 @@ func (server *HttpServer) initHandlers() {
 // creates the ServeMux, inits the route handlers, and starts the server
 
 func (server *HttpServer) StartServer() {
-	server.Mux = http.NewServeMux()
+	server.mux = http.NewServeMux()
 
 	server.initHandlers()
 
 	log.Print("Server started...")
-	log.Fatal(http.ListenAndServe(":8000", server.Mux))
+
+	log.Fatal(http.ListenAndServe(":8000", server.mux))
+
 }
