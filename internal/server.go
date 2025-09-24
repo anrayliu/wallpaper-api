34d4fb215bd5a24410077c9468a8c07cb91fd85e
Anray Liu <anrayliu@gmail.com>
2025-09-23 23:27:37 -0400

    feat(server): serve images from azure blob storage

--- internal/server.go
+++ internal/server.go
@@ -1,9 +1,14 @@
+// reverse proxy http server to serve images from azure blob storage through a restful api
+
 package internal
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func renderJSON[T ResponseError | ResponseSuccess](w http.ResponseWriter, response T) {
@@ -16,12 +21,44 @@ type HttpServer struct {
 	mux *http.ServeMux
 }
 
+// proxies azure blob storage by downloading image with SAS URI and
+// writing into response
+
+func fetchBlob(w http.ResponseWriter, url string) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		renderJSON(w, NewResponse502())
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		renderJSON(w, NewResponse404())
+		return
+	}
+
+	for k, v := range resp.Header {
+		w.Header()[k] = v
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		renderJSON(w, NewResponse500())
+	}
+
+}
+
 // sets the route handlers
 
 func (server *HttpServer) initHandlers() {
 	server.mux.HandleFunc("/get/{file}/", func(w http.ResponseWriter, r *http.Request) {
 		fileName := r.PathValue("file")
-		renderJSON(w, ResponseSuccess{"hello world: " + fileName})
+		imagePath := fmt.Sprintf("%s%s?%s", os.Getenv("SAS_URI"), fileName, os.Getenv("SAS_TOKEN"))
+
+		fetchBlob(w, imagePath)
 	})
 
 	server.mux.HandleFunc("/get/", func(w http.ResponseWriter, r *http.Request) {
